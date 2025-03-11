ea4457cff6d25b525ee7aad07941fe8a12fbb992
Aditya Kumar Singh <50352988+TheMartian0x48@users.noreply.github.com>
2025-03-11 21:40:48 +0530

    Update lexer.zig

--- src/lexer.zig
+++ src/lexer.zig
@@ -60,22 +60,52 @@ pub const Lexer = struct {
         }
     }
 
+    pub fn peekChar(l: *Lexer) u8 {
+        if (l.readPosition >= l.input.len) {
+            return 0;
+        } else {
+            return l.input[l.readPosition];
+        }
+    }
+
     pub fn nextToken(l: *Lexer) token.Token {
         var tok: token.Token = undefined;
 
         l.skipWhiteSpace();
 
         switch (l.ch) {
-            '=' => tok = token.Token.new(token.Tag.assign),
+            '=' => {
+                if (l.peekChar() == '=') {
+                    const lexeme = "==";
+                    l.readChar();
+                    tok = token.Token.create(token.Tag.eq, lexeme);
+                } else {
+                    tok = token.Token.new(token.Tag.assign);
+                }
+            },
             ';' => tok = token.Token.new(token.Tag.semicolon),
             ',' => tok = token.Token.new(token.Tag.comma),
-
             '(' => tok = token.Token.new(token.Tag.lparen),
             ')' => tok = token.Token.new(token.Tag.rparen),
             '{' => tok = token.Token.new(token.Tag.lbrace),
             '}' => tok = token.Token.new(token.Tag.rbrace),
 
             '+' => tok = token.Token.new(token.Tag.plus),
+            '-' => tok = token.Token.new(token.Tag.minus),
+
+            '*' => tok = token.Token.new(token.Tag.asterisk),
+            '!' => {
+                if (l.peekChar() == '=') {
+                    const lexeme = "!=";
+                    l.readChar();
+                    tok = token.Token.create(token.Tag.not_eq, lexeme);
+                } else {
+                    tok = token.Token.new(token.Tag.bang);
+                }
+            },
+            '<' => tok = token.Token.new(token.Tag.lt),
+            '>' => tok = token.Token.new(token.Tag.gt),
+            '/' => tok = token.Token.new(token.Tag.slash),
 
             0 => tok = token.Token.new(token.Tag.eof),
             else => {
@@ -124,58 +154,165 @@ pub const Lexer = struct {
 //     }
 // }
 
-test "test code 2" {
-    token.init();
+// test "test code 2" {
+//     token.init();
+//     const input: []const u8 =
+//         \\ let five = 5;
+//         \\ let ten = 10;
+//         \\ let add = fn(x, y) {
+//         \\     x + y;
+//         \\ };
+//         \\ let result = add(five, ten);
+//     ;
+//
+//     const tests = [_]token.Token{
+//         token.Token.new(token.Tag.let),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.assign),
+//         token.Token.new(token.Tag.int),
+//         token.Token.new(token.Tag.semicolon),
+//
+//         token.Token.new(token.Tag.let),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.assign),
+//         token.Token.new(token.Tag.int),
+//         token.Token.new(token.Tag.semicolon),
+//
+//         token.Token.new(token.Tag.let),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.assign),
+//         token.Token.new(token.Tag.function),
+//         token.Token.new(token.Tag.lparen),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.comma),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.rparen),
+//         token.Token.new(token.Tag.lbrace),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.plus),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.semicolon),
+//         token.Token.new(token.Tag.rbrace),
+//         token.Token.new(token.Tag.semicolon),
+//
+//         token.Token.new(token.Tag.let),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.assign),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.lparen),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.comma),
+//         token.Token.new(token.Tag.ident),
+//         token.Token.new(token.Tag.rparen),
+//         token.Token.new(token.Tag.semicolon),
+//
+//         token.Token.new(token.Tag.eof),
+//     };
+//     var l = Lexer.new(input);
+//
+//     for (tests, 0..) |t, i| {
+//         std.debug.print("Test token #{d} : {any}\n", .{ i, t.tag.lexeme() });
+//         const tok = l.nextToken();
+//         std.testing.expectEqual(t.tag, tok.tag) catch |err| {
+//             token.deinit();
+//             return err;
+//         };
+//     }
+//     token.deinit();
+// }
+//
+
+test "test code 3" {
     const input: []const u8 =
-        \\ let five = 5;
-        \\ let ten = 10;
-        \\ let add = fn(x, y) {
-        \\     x + y;
-        \\ };
-        \\ let result = add(five, ten);
+        \\ !-/*5;
+        \\ 5 < 10 > 5;
     ;
 
     const tests = [_]token.Token{
-        token.Token.new(token.Tag.let),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.assign),
+        token.Token.new(token.Tag.bang),
+        token.Token.new(token.Tag.minus),
+        token.Token.new(token.Tag.slash),
+        token.Token.new(token.Tag.asterisk),
+        token.Token.new(token.Tag.int),
+        token.Token.new(token.Tag.semicolon),
+        token.Token.new(token.Tag.int),
+        token.Token.new(token.Tag.lt),
+        token.Token.new(token.Tag.int),
+        token.Token.new(token.Tag.gt),
         token.Token.new(token.Tag.int),
         token.Token.new(token.Tag.semicolon),
 
-        token.Token.new(token.Tag.let),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.assign),
+        token.Token.new(token.Tag.eof),
+    };
+    var l = Lexer.new(input);
+
+    for (tests, 0..) |t, i| {
+        std.debug.print("Test token #{d} : {any}\n", .{ i, t.tag.lexeme() });
+        const tok = l.nextToken();
+        try std.testing.expectEqual(tok.tag, t.tag);
+    }
+}
+
+// test "test code 4" {
+//     const input: []const u8 =
+//         \\ if(5 < 10) {
+//         \\   return true;
+//         \\ } else {
+//         \\   return false;
+//         \\ }
+//     ;
+//     token.init();
+//
+//     const tests = [_]token.Token{
+//         token.Token.new(token.Tag.ifT),
+//         token.Token.new(token.Tag.lparen),
+//         token.Token.new(token.Tag.int),
+//         token.Token.new(token.Tag.lt),
+//         token.Token.new(token.Tag.int),
+//         token.Token.new(token.Tag.rparen),
+//         token.Token.new(token.Tag.lbrace),
+//         token.Token.new(token.Tag.returnT),
+//         token.Token.new(token.Tag.trueT),
+//         token.Token.new(token.Tag.semicolon),
+//         token.Token.new(token.Tag.rbrace),
+//         token.Token.new(token.Tag.elseT),
+//         token.Token.new(token.Tag.lbrace),
+//         token.Token.new(token.Tag.returnT),
+//         token.Token.new(token.Tag.falseT),
+//         token.Token.new(token.Tag.semicolon),
+//         token.Token.new(token.Tag.rbrace),
+//
+//         token.Token.new(token.Tag.eof),
+//     };
+//     var l = Lexer.new(input);
+//
+//     for (tests, 0..) |t, i| {
+//         std.debug.print("Test token #{d} : {any}\n", .{ i, t.tag.lexeme() });
+//         const tok = l.nextToken();
+//         std.testing.expectEqual(t.tag, tok.tag) catch |err| {
+//             token.deinit();
+//             return err;
+//         };
+//     }
+//     token.deinit();
+// }
+
+test "test code 5" {
+    const input: []const u8 =
+        \\ 10 == 10;
+        \\ 9 != 10;
+    ;
+    token.init();
+
+    const tests = [_]token.Token{
+        token.Token.new(token.Tag.int),
+        token.Token.new(token.Tag.eq),
         token.Token.new(token.Tag.int),
         token.Token.new(token.Tag.semicolon),
-
-        token.Token.new(token.Tag.let),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.assign),
-        token.Token.new(token.Tag.function),
-        token.Token.new(token.Tag.lparen),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.comma),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.rparen),
-        token.Token.new(token.Tag.lbrace),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.plus),
-        token.Token.new(token.Tag.ident),
+        token.Token.new(token.Tag.int),
+        token.Token.new(token.Tag.not_eq),
+        token.Token.new(token.Tag.int),
         token.Token.new(token.Tag.semicolon),
-        token.Token.new(token.Tag.rbrace),
-        token.Token.new(token.Tag.semicolon),
-
-        token.Token.new(token.Tag.let),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.assign),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.lparen),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.comma),
-        token.Token.new(token.Tag.ident),
-        token.Token.new(token.Tag.rparen),
-        token.Token.new(token.Tag.semicolon),
-
         token.Token.new(token.Tag.eof),
     };
     var l = Lexer.new(input);
