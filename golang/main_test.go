24a2a7c824fb845fd767dc7dab4cea69f60bebfa
Aditya Kumar Singh <cr7.aditya.cs@gmail.com>
2025-03-13 01:13:39 +0530

    interpreter

---
+++ golang/main_test.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestNextToken(t *testing.T) {
+	input := `=+(){},;`
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{ASSIGN, "="},
+		{PLUS, "+"},
+		{LPAREN, "("},
+		{RPAREN, ")"},
+		{LBRACE, "{"},
+		{RBRACE, "}"},
+		{COMMA, ","},
+		{SEMICOLON, ";"},
+		{EOF, ""},
+	}
+	l := NewLexer(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - ype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+func TestNextTokenTwo(t *testing.T) {
+	input := `let five = 5;
+            let ten = 10;
+            let add = fn(x, y) {
+                x + y;
+            };
+            let result = add(five, ten);
+`
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{LET, "let"},
+		{IDENT, "five"},
+		{ASSIGN, "="},
+		{INT, "5"},
+		{SEMICOLON, ";"},
+		{LET, "let"},
+		{IDENT, "ten"},
+		{ASSIGN, "="},
+		{INT, "10"},
+		{SEMICOLON, ";"},
+		{LET, "let"},
+		{IDENT, "add"},
+		{ASSIGN, "="},
+		{FUNCTION, "fn"},
+		{LPAREN, "("},
+		{IDENT, "x"},
+		{COMMA, ","},
+		{IDENT, "y"},
+		{RPAREN, ")"},
+		{LBRACE, "{"},
+		{IDENT, "x"},
+		{PLUS, "+"},
+		{IDENT, "y"},
+		{SEMICOLON, ";"},
+		{RBRACE, "}"},
+		{SEMICOLON, ";"},
+		{LET, "let"},
+		{IDENT, "result"},
+		{ASSIGN, "="},
+		{IDENT, "add"},
+		{LPAREN, "("},
+		{IDENT, "five"},
+		{COMMA, ","},
+		{IDENT, "ten"},
+		{RPAREN, ")"},
+		{SEMICOLON, ";"},
+		{EOF, ""},
+	}
+	l := NewLexer(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - ype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+func TestNextTokenThree(t *testing.T) {
+	input := `!-/*5;
+    5 < 10 > 5;
+`
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{BANG, "!"},
+		{MINUS, "-"},
+		{SLASH, "/"},
+		{ASTRISK, "*"},
+		{INT, "5"},
+		{SEMICOLON, ";"},
+		{INT, "5"},
+		{LT, "<"},
+		{INT, "10"},
+		{GT, ">"},
+		{INT, "5"},
+		{SEMICOLON, ";"},
+		{EOF, ""},
+	}
+	l := NewLexer(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - ype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
+
+func TestNextTokenFour(t *testing.T) {
+	input := `if (5 < 10) {
+        return true;
+    } else {
+        return false;
+    }
+`
+	tests := []struct {
+		expectedType    TokenType
+		expectedLiteral string
+	}{
+		{IF, "if"},
+		{LPAREN, "("},
+		{INT, "5"},
+		{LT, "<"},
+		{INT, "10"},
+		{RPAREN, ")"},
+		{LBRACE, "{"},
+		{RETURN, "return"},
+		{TRUE, "true"},
+		{SEMICOLON, ";"},
+		{RBRACE, "}"},
+		{ELSE, "else"},
+		{LBRACE, "{"},
+		{RETURN, "return"},
+		{FALSE, "false"},
+		{SEMICOLON, ";"},
+		{RBRACE, "}"},
+		{EOF, ""},
+	}
+	l := NewLexer(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - ype wrong. expected=%q, got=%q",
+				i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
+				i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
